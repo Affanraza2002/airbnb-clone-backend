2e52f825621560cbd375851f1839653e15e32c85
MdKaif001 <mkkaif002@gmail.com>
2024-04-21 20:27:18 +0530

    Affan | Added full code of backend

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const cloudinary = require('cloudinary').v2;
 const bodyParser = require("body-parser");
 const allRoutes = require('./routes')
 
-// connect with database
-connectWithDB();
+// // connect with database
+// connectWithDB();
 
-// cloudinary configuration          
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-});
+// // cloudinary configuration          
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET
+// });
 
 const app = express();
 
@@ -28,14 +28,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Initialize cookie-session middleware
-app.use(cookieSession({
-  name: 'session',
-  maxAge: process.env.COOKIE_TIME * 24 * 60 * 60 * 1000,
-  keys: [process.env.SESSION_SECRET],
-  secure: true, // Only send over HTTPS
-  sameSite: 'none', // Allow cross-origin requests
-  httpOnly: true, // Makes the cookie accessible only on the server-side
-}))
+// app.use(cookieSession({
+//   name: 'session',
+//   maxAge: process.env.COOKIE_TIME * 24 * 60 * 60 * 1000,
+//   keys: [process.env.SESSION_SECRET],
+//   secure: true, // Only send over HTTPS
+//   sameSite: 'none', // Allow cross-origin requests
+//   httpOnly: true, // Makes the cookie accessible only on the server-side
+// }))
 
 // middleware to handle json
 
@@ -54,11 +54,9 @@ app.use('/', (req, res) =>{
     res.send('Hello from server side');
 });
 
-app.listen(process.env.PORT || 8000, (err) => {
-  if (err) {
-    console.log('Error in connecting to server: ', err);
-  }
-  console.log(`Server is running on port no. ${process.env.PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
 });
 
 module.exports = app;
