a7149ee0d48912a29a506bd9d7a1c6cfb583210f
MdKaif001 <mkkaif002@gmail.com>
2024-04-21 19:55:05 +0530

    Affan | Added full code of backend

--- index.js
+++ index.js
@@ -57,7 +57,18 @@
 
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
