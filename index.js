853325a6a69dbd6d4b7a4f49685c600593f94a74
MdKaif001 <mkkaif002@gmail.com>
2024-04-21 20:36:54 +0530

    Affan | Added full code of backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const connectWithDB = require('./config/db');
-// const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session')
 const cookieParser = require('cookie-parser')
 const cloudinary = require('cloudinary').v2;
 const bodyParser = require("body-parser");
@@ -29,14 +29,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Initialize cookie-session middleware
-// app.use(cookieSession({
-//   name: 'session',
-//   maxAge: process.env.COOKIE_TIME * 24 * 60 * 60 * 1000,
-//   keys: [process.env.SESSION_SECRET],
-//   secure: true, // Only send over HTTPS
-//   sameSite: 'none', // Allow cross-origin requests
-//   httpOnly: true, // Makes the cookie accessible only on the server-side
-// }))
+app.use(cookieSession({
+  name: 'session',
+  maxAge: process.env.COOKIE_TIME * 24 * 60 * 60 * 1000,
+  keys: [process.env.SESSION_SECRET],
+  secure: true, // Only send over HTTPS
+  sameSite: 'none', // Allow cross-origin requests
+  httpOnly: true, // Makes the cookie accessible only on the server-side
+}))
 
 // middleware to handle json
 
